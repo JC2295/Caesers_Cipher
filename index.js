1bcf3c11759fb5611a1c9eea592664865c945ed0
JC2295 <64856330+JC2295@users.noreply.github.com>
2023-06-21 11:09:55 +0000

    Progress

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 function rot13(str) {
 
+  let inputArr = str.split("");
+
+  let outputArr = [];
+  
+  
   let alpha = ["A", "B", "C","D","E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"];
 
-  let alpha13 = ["L", "M", "N","O","P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+  // A > N
+  // B > O
+  
+ let alpha13 = ["N", "O", "P","Q","R","S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M"];
   
   
+  for(let i = 0; i < inputArr.length; i++){
+
+    for(let j = 0; j < 26; j++){
+
+      if(inputArr[i] === alpha[j]){
+
+        outputArr.push(alpha13[j]);
+        
+      }
+      else{
+        
+        continue;
+      }
+      
+    }
+    
+  }
+
+  let outputStr = outputArr.join("");
+
+  console.log(outputStr);
   
-  return str;
+  return outputStr;
 }
 
 rot13("SERR PBQR PNZC");
